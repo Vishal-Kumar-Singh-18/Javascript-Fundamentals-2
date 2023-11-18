8a47a28f667e5c0f54a1ce7ec9039637c1fd525c
Vishal Kumar Singh <144977148+Vishal-Kumar-Singh-18@users.noreply.github.com>
2023-11-18 23:38:21 +0530

    Assignment Submitted

---
+++ Assignment/q1.js
@@ -0,0 +1,9 @@
+// Program to check if a number is even or odd using the ternary operator
+
+var number = 7;
+
+// Using the ternary operator to determine if the number is even or odd
+var result = number % 2 === 0 ? "even" : "odd";
+
+// Displaying the result
+console.log("The number " + number + " is " + result + ".");
---
+++ Assignment/q3.js
@@ -0,0 +1,8 @@
+/* Use a nested ternary operator to check that a number is positive, negative or zero. You have to print 
+“positive” if the number is positive and similarly for negative and zero also */
+
+let number = 42; 
+
+let result = number > 0 ? "positive" : number < 0 ? "negative" : "zero";
+
+console.log(result);
